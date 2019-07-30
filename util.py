e96c3b9b802fcd5afa52da2e62d43d47bcbd9312
jesse <jessedp@gmail.com>
2019-07-30 12:51:29 -0400

    fix line spacing

--- util.py
+++ util.py
@@ -11,9 +11,8 @@ def chunks(l, n):
     for i in range(0, len(l), n):
         yield l[i:i + n]
 
+
 # I feel like there are exceptions to be handled here
-
-
 def convert_datestr(str, fmt='%m/%d/%Y %H:%M'):
     x = parse(str)
     tz = config.get('General', 'Timezone')
